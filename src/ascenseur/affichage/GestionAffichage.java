12320e681db439728f658f4b5640897cafb7d4db
Mathieu HP <mathieu.herranz-perez@etu.univ-amu.fr>
2015-12-31 13:23:50 +0100

    + afficherAscenseur()

--- src/ascenseur/affichage/GestionAffichage.java
+++ src/ascenseur/affichage/GestionAffichage.java
@@ -2,7 +2,7 @@ package ascenseur.affichage;
 
 import java.util.Scanner;
 
-public class GestionAffichage { // FAUDRA GERER LES EXCEPTIONS
+public class GestionAffichage { // FAUDRA GERER LES EXCEPTIONS !
 		
 		final static char ASCENSEUR_EN_DESCENTE = 'D';
 		final static char ASCENSEUR_EN_MONTE = 'M';
@@ -21,36 +21,42 @@ public class GestionAffichage { // FAUDRA GERER LES EXCEPTIONS
 			System.out.println("Nb ascenseur : ");
 			int nbAscenseur = sc.nextInt();
 			
-			char[][] immeuble = new char[(nbAscenseur * 2) + 1][nbEtage - 1]; 
-			for (int i = 0; i < immeuble.length - 1; ++i) {					//initialiser les étages
+			char[][] immeuble = new char[nbEtage + 1][(nbAscenseur * 2) + 2];
+			for (int i = 0; i < immeuble.length - 1; ++i) {					//initialiser les Ã©tages
 				for (int j = 0; j < immeuble[i].length - 1; ++j) {
 					immeuble[i][j] = ETAGE; 
 				}
 			}
-			for (int i = immeuble.length - 1; i < immeuble.length; ++i) {	//initialiser les ascenseurs fermés
-				for (int j = 0; j < immeuble[i].length - 1; ++j) {			// à l'étage 0
+
+			for (int i = immeuble.length - 1; i < immeuble.length; ++i) {	//initialiser les ascenseurs fermÃ©s
+				for (int j = 0; j < immeuble[i].length - 1; ++j) {			// Ã  l'Ã©tage 0
 					if (j % 2 == 0)
 						immeuble[i][j] = ASCENSEUR_FERME; 
 					else
 						immeuble[i][j] = ETAGE;
 				}
 			}
-			for (int i = 0; i < immeuble.length; ++i) {						//initialiser les boutons d'appel à 0
-				for (int j = immeuble[i].length - 1; j < immeuble[i].length; ++j) {
+			
+			for (int i = 0; i < immeuble.length; ++i) {						//initialiser les boutons d'appel Ã  0
+				for (int j = immeuble[i].length - 2; j < immeuble[i].length; ++j) {
 					immeuble[i][j] = PAS_APPEL;
 				}
 			}
+			
+			return immeuble;
+		}
+		
+		private static void afficherImmeuble(char[][] immeuble) {
 			for (int i = 0; i < immeuble.length; ++i) {						//afficher l'immeuble
 				for (int j = 0; j < immeuble[i].length; ++j) {
 					System.out.print(immeuble[i][j]);
 				}
 				System.out.println('\r');
 			}
-			
-			return immeuble;
 		}
 		
 		public static void main(String[] args) {
 			char[][] im = preparerImmeuble();
+			afficherImmeuble(im);
 		}
 }
